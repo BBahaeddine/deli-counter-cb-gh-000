a9babdfa3fbd0cccd16da41ef5f43fb2fce39c64
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-09 10:01:40 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 katz_deli = []
 def line
-  if line.size == 0
+  if katz_deli.size == 0
     return "The line is currently empty."
   else
     msg = "The line is currently: "
