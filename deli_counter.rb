2865b193408e2cae3582946c5d656a194a4f780a
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-09 10:04:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,9 @@
 # Write your code here.
 katz_deli = []
-def line(katz_deli)
-  if katz_deli.size == 0
-    return "The line is currently empty."
-  else
-    msg = "The line is currently: "
-    katz_deli.each_with_index {|name, index|
-      position = index + 1 
-      msg << "#{msg} #{position}. #{name}"
-    }
-    return msg
-  end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  position = katz_deli.size + 1
+  puts
+  
 end
